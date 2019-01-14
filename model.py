48bbee9ddbdf72271b49efda957fec793ef8c6ba
mmastrelli <mirko.mastrelli@bit-libre.com>
2019-01-14 13:04:41 +0100

    Submission

--- model.py
+++ model.py
@@ -13,21 +13,49 @@ class EncoderCNN(nn.Module):
         modules = list(resnet.children())[:-1]
         self.resnet = nn.Sequential(*modules)
         self.embed = nn.Linear(resnet.fc.in_features, embed_size)
+        self.bn = nn.BatchNorm1d(embed_size, momentum=0.01)
 
     def forward(self, images):
         features = self.resnet(images)
         features = features.view(features.size(0), -1)
-        features = self.embed(features)
+        features = self.bn(self.embed(features))
         return features
     
 
 class DecoderRNN(nn.Module):
-    def __init__(self, embed_size, hidden_size, vocab_size, num_layers=1):
-        pass
+    def __init__(self, embed_size, hidden_size, vocab_size, num_layers=2):
+        super(DecoderRNN, self).__init__()
+        self.hidden_size = hidden_size
+        self.num_layers = num_layers
+        self.embed = nn.Embedding(vocab_size, embed_size)
+        self.lstm = nn.LSTM(embed_size, hidden_size, num_layers, batch_first=True)
+        self.linear = nn.Linear(hidden_size, vocab_size)
+        self.init_weights()
+        
+    def init_weights(self):
+        
+        self.embed.weight.data.uniform_(-0.1, 0.1)
+        self.linear.weight.data.uniform_(-0.1, 0.1)
+        self.linear.bias.data.fill_(0)
     
     def forward(self, features, captions):
-        pass
+        
+        cap_embed = self.embed(captions[:, :-1])  
+        embeddings = torch.cat((features.unsqueeze(1), cap_embed), dim=1) 
+        hidden, _ = self.lstm(embeddings)  
+        outputs = self.linear(hidden)  
+        return outputs
 
     def sample(self, inputs, states=None, max_len=20):
-        " accepts pre-processed image tensor (inputs) and returns predicted sentence (list of tensor ids of length max_len) "
-        pass
+        " accepts pre-processed image tensor (inputs) and returns predicted sentence (list of tensor ids of length max_len) " 
+        samples = []
+        for i in range(max_len):
+            hiddens, states = self.lstm(inputs, states)
+            outputs = self.linear(hiddens.squeeze(1))
+            predicted = outputs.max(1)[1]
+            samples.append(predicted.item())
+            inputs = self.embed(predicted)
+            inputs = inputs.unsqueeze(1)
+        return samples
+        
+        
